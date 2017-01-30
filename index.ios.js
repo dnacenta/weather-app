a17637e8073a9a120367182796940b2f80be6671
Vader0 <vader0@protonmail.com>
2017-01-30 20:41:09 +0100

    Design

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  StatusBar
+  } from 'react-native';
 
-export default class coolWinter extends Component {
-  render() {
-    return (
+import Icon from 'react-native-vector-icons/Ionicons'
+
+class App extends Component {
+
+  render(){
+    return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <StatusBar hidden={true}/>
+        <View style={styles.header}>
+          <Icon name={'ios-snow'} size={80} color={'white'}/>
+          <Text style={styles.temp}>9º</Text>
+        </View>
+        <View style={styles.body}>
+          <Text style={styles.title}>
+            This a <Text style={{color:'#F7DC6F'}}>Fucking</Text> Weather App
+          </Text>
+          <Text style={styles.subtitle}>Let It Snow</Text>
+        </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex:1,
+    backgroundColor:'#00AEF9'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent:'space-around',
+    flex:1,
+    // backgroundColor:'#F7DC6F'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  temp: {
+    fontFamily: 'HelveticaNeue-Bold',
+    fontSize: 70,
+    color: 'white'
   },
-});
+  body: {
+    alignItems:'flex-start',
+    justifyContent:'flex-end',
+    flex:5,
+    // backgroundColor:'#00AEF9',
+    margin:10
+  },
+  title: {
+    fontFamily: 'HelveticaNeue-Bold',
+    fontSize: 78,
+    color: 'white',
+    marginBottom:5
+  },
+  subtitle:{
+    fontFamily: 'HelveticaNeue-Medium',
+    fontSize: 16,
+    color: 'white'
+  }
+})
 
-AppRegistry.registerComponent('coolWinter', () => coolWinter);
+AppRegistry.registerComponent('coolWinter', () => App)
